alanhogan/Add-Another

commit 1a4ba7bed602d92f98504545fe90ae4b92f9a034

Fixed bug that erroneously ate parts of certain names which also reliably triggered a crash in Chrome 5.0.375.125. If the repeatable section had two fields, one named foo[apple][] and one named foo[banana][], both would become foo[0][].

addAnother.jquery.js
@@ -1,8 +1,8 @@
 // AddAnother jQuery Plugin by Jeremy Lindblom. Based on relCopy by Andres Vidal.
 (function($) {
 	
-	var regexSingleBrackets = /\[([^\]]*)\]+$/;
-	var regexDoubleBrackets = /\[[^\]]*\]\[([^\]]*)\]+$/;
+	var regexSingleBrackets = /\[0?\]+$/;
+	var regexDoubleBrackets = /\[0?\]\[([^\]])\]+$/;
 
 	// Create the addAnother jQuery plugin
 	$.fn.addAnother = function(options) {

commit dfef637e267f7b6fdf9e915fc6acabc4125a3f96

Quick improvement to the double-bracket detection. Now more strict, and also corrects a bug that would prevent matching in a number of situations, introduced in 1a4ba7bed602d92f98504545fe90ae4b92f9a034

addAnother.jquery.js
@@ -2,7 +2,7 @@
 (function($) {
 	
 	var regexSingleBrackets = /\[0?\]+$/;
-	var regexDoubleBrackets = /\[0?\]\[([^\]])\]+$/;
+	var regexDoubleBrackets = /\[0\]\[([^\]]*)\]+$/;
 
 	// Create the addAnother jQuery plugin
 	$.fn.addAnother = function(options) {