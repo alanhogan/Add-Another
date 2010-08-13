5d2fe582d0d1c19e106f00780f1547e6e51e0a2d
Alan Hogan <contact@alanhogan.com>
2010-08-13 15:07:46 -0700

    Improvements: 1. Now supports a 'minimum' number of fields to keep from deleting (this does not trigger auto-add on load, though), especially relevant with: 2. Now supports multiple instances af the repeatable section having existed in the markup (server-side) without replicating all of them every time an Add Another link is clicked, without showing too many such links, etc. Note this support is still young and rough; certain issues are to be expected such as increased chance of ID collision. 3. Utilization of the jQuery .live() method for adding behavior to 'remove' links. 4. You can nowspecify selectors to apply add and remove behavior to elements you yourself added to the DOM, and 5. you can now prevent AddAnother from adding add and remove elements for you automatically.  6. (BEHAVIOR CHANGE) It now does not futz with the names of input elements by default  (you can still set explicitBracketNumbering to true). 7. README changes to document changed options.

--- README.markdown
+++ README.markdown
@@ -15,15 +15,23 @@ multiple file uploaders.
 ## Settings
 
 - **limit** - The number of items that can exist in the group. 0 = unlimited
+- **minimum** - The minimum number of items that can exist in the group. Default 1. 
+	This affects the "remove" functionality only, and does not trigger auto-add on load.
 - **excludeSelector** - Elements with this class will not be cloned
 - **emptySelector** - Elements with this class will have subelements removed
 - **clearInputs** - If true, then cloned input fields will have an empty value
 - **animate** - If true, show/hide funcion are used with animation
 - **allowRemove** - If true, then a remove link will be added automatically
-- **removeClass** - The class of the remove link
-- **addLinkText** - The text of the add another link
-- **addLinkClass** - The class of the add another link 
-	(by default, `add-another-x` where `.x` appears in selector used to indicate what will be duplicated.)
+- **removeClass** - The class of the remove link as a selector. (Should *not* begin with a period)
+- **removeSelector** - If you are inserting your own remove links into the DOM, this selector
+	lets you apply behavior to them.
+- **addLinkText** - The <del>text</del> <ins>innerHTML</ins> of the add another link, or `false` to prevent the link from being added (use)
+- **addLinkClass** - The class of the add another link. (Should *not* begin with a period.)
+	(By default, `add-another-x` where `.x` appears in selector used to indicate what will be duplicated.)
+- **addLinkSelector** - Adds the same behavior as the link created by `addLinkText`/`addLinkClass` to the element(s),
+	if any, specified by this string.
+	Note this is necessary if `addLinkText` is `false` then this does _not_ need to be a class; rather it
+	can be any jQuery-supported selector, such as `"#foo .bar:last"`.
 - **removeLinkText** - The text of the remove link
 - **onFull** - A callback to be executed after an item is added and the group is full
 - **onNotFull** - A callback to be executed after an item is added and the group is NOT full
@@ -34,7 +42,8 @@ multiple file uploaders.
 	and names that ended in    
 	`[0][foo]` to    
 	`[0][foo]`, `[1][foo]`, `[2][foo]`…, respectively.    
-	This is `true` by default.    
+	This is `false` by default, and it can cause problems if you repeat sections server-side as well
+	in the current implementation.    
 	(Don't worry, copied elements' `id` attributes and corresponding `for` attributes 
 	are appended with `_1`, `_2`, … regardless.)
 
--- addAnother.jquery.js
+++ addAnother.jquery.js
@@ -22,26 +22,29 @@
 		// Take the options parameter and combine with default settings
 		var settings = jQuery.extend({
 			limit: 0,
+			minimum: 1,
 			excludeSelector: ".exclude-item",
 			emptySelector: ".empty-item",
 			clearInputs: true,
 			animate: false,
 			allowRemove: true,
 			removeClass: 'remove-item',
+			removeSelector: false,
 			addLinkText: "Add Another",
 			addLinkClass: 'add-another-'+itemClass,
+			addLinkSelector: false,
 			removeLinkText: "Remove",
 			onFull: false,
 			onNotFull: false,
 			onRemove: false,
-			explicitBracketNumbering: true
+			explicitBracketNumbering: false
 		}, options);
 		
 		// Make sure limit is an int
 		settings.limit = parseInt(settings.limit);
 
 		// Add Another Link
-		$(this).after('<div class="add-another"><a href="#" class="'+settings.addLinkClass+'">'+settings.addLinkText+'</a></div>');
+		if (settings.addLinkText) $(itemSelector + ':last').after('<div class="add-another"><a href="#" class="'+settings.addLinkClass+'">'+settings.addLinkText+'</a></div>');
 		
 		// Fix initial bracketed values of input names for array submittal
 		if (settings.explicitBracketNumbering) {
@@ -56,143 +59,153 @@
 			});
 		}
 		
-		// Loop each element
-		this.each(function() {
-			
-			// Set click action
-			$('.add-another-'+itemClass).click(function(){
-				
-				// Stop at limit
-				if (settings.limit != 0 && $(itemSelector).length >= settings.limit){
-					return false;
-				};
-
-				// Increment the element counter
-				if (!counters[itemSelector]){
-					counters[itemSelector] = 1;
-				}
-				var counter = counters[itemSelector]++;
-				
-				// Get jQuery objects for all of the participating elements
-				var master = $(itemSelector+":first");
-				var parent = $(master).parent();
-				var clone = $(master).clone(true)
-					.addClass(copyClass+counter);
-
-				// Add a remove link if enabled
-				if (settings.allowRemove){
-					$(clone).append('<a href="#" class="'+settings.removeClass+'">'+settings.removeLinkText+'</a>');
-				}
-				
-				// Remove elements with excludeSelector from the clone
-				if (settings.excludeSelector){
-					$(clone).find(settings.excludeSelector).remove();
-				};
-				
-				// Empty elements with emptySelector in the clone
-				if (settings.emptySelector){
-					$(clone).find(settings.emptySelector).empty();
-				};								
-				
-				// Increment clone IDs
-				if ($(clone).attr('id') ){
-					var newid = $(clone).attr('id')+'_'+counter;
-					$(clone).attr('id', newid);
-				};
-				
-				// Increment clone children IDs
-				$(clone).find('[id]').each(function(){
-					var newid = $(this).attr('id')+'_'+counter;
-					$(this).attr('id', newid);
-				});
-				
-				// Increment clone children labels
-				$(clone).find('[for]').each(function(){
-					var newfor = $(this).attr('for')+'_'+counter;
-					$(this).attr('for', newfor);
-				});
-				
-				// Fix bracketed values of input names for array submittal
-				if (settings.explicitBracketNumbering) {
-					$(clone).find('input, textarea, select').each(function(){
-						var elementName = $(this).attr('name');
-						if (regexDoubleBrackets.test(elementName)){
-							elementName = elementName.replace(regexDoubleBrackets, '['+counter+'][$1]');
-						} else if (regexSingleBrackets.test(elementName)){
-							elementName = elementName.replace(regexSingleBrackets, '['+counter+']');
-						}
-						$(this).attr('name', elementName);
+		// TODO let user customize
+		var addRemoveLink = function ($to) {
+			$to.append('<a href="#" class="'+settings.removeClass+'">'+settings.removeLinkText+'</a>');
+		};
+		
+		// If more repeating sections are present than the minimum, give ’em remove links.
+		if (settings.allowRemove) this.each(function(i,elem) {
+			if (i >= settings.minimum || settings.minimum === 0) addRemoveLink($(elem));
+		});
+		
+					
+		// Add remove operation to removeClass click event
+		if (settings.allowRemove && settings.removeClass) {
+			$('.'+settings.removeClass+(settings.removeSelector ? ', '+settings.removeSelector : '')).live('click', function(){
+				var $remove = $(this).parents(itemSelector);
+				// Remove the copy of which the remove link was clicked
+				if (settings.animate){ //todo allow speed to be set configurably
+					$remove.hide('fast', function(){
+						$remove.remove();
 					});
-				}
-				
-				// Clear Inputs/Textarea
-				if (settings.clearInputs){
-					$(clone).find(':input').each(function(){
-						switch ($(this).attr('type'))
-						{
-							case "button":
-							case "reset":
-							case "submit":
-								break;
-							case "checkbox":
-							case "radio":
-								$(this).attr('checked', '');
-								break;
-							default:
-							  $(this).val("");
-						}
-					});
-				};
-				
-				// Hide new item by default if going to animate
-				if (settings.animate){
-					$(clone).css('display', 'none');
-				}
-				
-				// Append the new element(s)
-				$(parent).find(itemSelector+':last').after(clone);
-				
-				// Show it with animation
-				if (settings.animate){
-					$(clone).show('fast');
-				}
-				
-				// Add remove operation to removeClass click event
-				if (settings.removeClass){
-					var removeTarget = '.'+copyClass+counter;
-					$(clone).find('.'+settings.removeClass).click(function(){
-						// Remove the copy of which the remove link was clicked
-						if (settings.animate){
-							$(removeTarget).hide('fast', function(){
-								$(removeTarget).remove();
-							});
-						}else{
-							$(removeTarget).remove();
-						}
-						
-						// Call the onRemove callback if defined
-						if (settings.onRemove){
-							settings.onRemove();
-						}
-					});
-				}
-				
-				// Run onFull/onNotFull callbacks if defined
-				if (settings.limit != 0 && $(itemSelector).length >= settings.limit){
-					if (settings.onFull){
-						settings.onFull();
-					}
 				}else{
-					if (settings.onNotFull){
-						settings.onNotFull();
-					}
+					$remove.remove();
 				}
-				
-				return false;
-				
-			}); // End click action
 			
-		}); // End each loop
+				// Call the onRemove callback if defined
+				if (settings.onRemove){
+					settings.onRemove();
+				}
+			});
+		}
+		
+		
+		// Set click action
+		$('.'+settings.addLinkClass+(settings.addLinkSelector ? ', '+settings.addLinkSelector : '')).click(function(){
+			
+			// Stop at limit
+			if (settings.limit != 0 && $(itemSelector).length >= settings.limit){
+				return false;
+			};
+
+			// Increment the element counter
+			if (!counters[itemSelector]){
+				counters[itemSelector] = 1;
+			}
+			var counter = counters[itemSelector]++;
+			
+			// Get jQuery objects for all of the participating elements
+			var master = $(itemSelector+":first");
+			var parent = $(master).parent();
+			var clone = $(master).clone(true)
+				.addClass(copyClass+counter);
+
+			// Add a remove link if enabled
+			if (settings.allowRemove){
+				if (counters[itemSelector] >= settings.minimum || settings.minimum === 0) {
+					if ($(clone).children('.'+settings.removeClass+(settings.removeSelector ? ', '+settings.removeSelector : '')).length === 0) { addRemoveLink($(clone)) };
+				}
+			}
+			
+			// Remove elements with excludeSelector from the clone
+			if (settings.excludeSelector){
+				$(clone).find(settings.excludeSelector).remove();
+			};
+			
+			// Empty elements with emptySelector in the clone
+			if (settings.emptySelector){
+				$(clone).find(settings.emptySelector).empty();
+			};								
+			
+			// Increment clone IDs
+			if ($(clone).attr('id') ){
+				var newid = $(clone).attr('id')+'_'+counter;
+				$(clone).attr('id', newid);
+			};
+			
+			// Increment clone children IDs
+			$(clone).find('[id]').each(function(){
+				var newid = $(this).attr('id')+'_'+counter;
+				$(this).attr('id', newid);
+			});
+			
+			// Increment clone children labels
+			$(clone).find('[for]').each(function(){
+				var newfor = $(this).attr('for')+'_'+counter;
+				$(this).attr('for', newfor);
+			});
+			
+			// Fix bracketed values of input names for array submittal
+			if (settings.explicitBracketNumbering) {
+				$(clone).find('input, textarea, select').each(function(){
+					var elementName = $(this).attr('name');
+					if (regexDoubleBrackets.test(elementName)){
+						elementName = elementName.replace(regexDoubleBrackets, '['+counter+'][$1]');
+					} else if (regexSingleBrackets.test(elementName)){
+						elementName = elementName.replace(regexSingleBrackets, '['+counter+']');
+					}
+					$(this).attr('name', elementName);
+				});
+			}
+			
+			// Clear Inputs/Textarea
+			if (settings.clearInputs){
+				$(clone).find(':input').each(function(){
+					switch ($(this).attr('type'))
+					{
+						case "button":
+						case "reset":
+						case "submit":
+							break;
+						case "checkbox":
+						case "radio":
+							$(this).attr('checked', '');
+							break;
+						default:
+						  $(this).val("");
+					}
+				});
+			};
+			
+			// Hide new item by default if going to animate
+			if (settings.animate){
+				$(clone).css('display', 'none');
+			}
+			
+			// Append the new element(s)
+			$(parent).find(itemSelector+':last').after(clone);
+			
+			// Show it with animation
+			if (settings.animate){
+				$(clone).show('fast');
+			}
+			
+			// Run onFull/onNotFull callbacks if defined
+			if (settings.limit != 0 && $(itemSelector).length >= settings.limit){
+				if (settings.onFull){
+					settings.onFull();
+				}
+			}else{
+				if (settings.onNotFull){
+					settings.onNotFull();
+				}
+			}
+			
+			return false;
+			
+		}); // End click action
+		
 		
 		return this; // Return to jQuery
 	};
